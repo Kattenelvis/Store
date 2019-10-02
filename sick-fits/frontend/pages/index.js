0c6aa2bc7abce805d014e3fb2ad2677145e1422c
Kattenelvis <officialkattenelvis@gmail.com>
2019-10-02 19:58:38 +0200

    Client Side

--- sick-fits/frontend/pages/index.js
+++ sick-fits/frontend/pages/index.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import Link from 'next/link'
 
 export default function index() {
     return (
         <div>
-            Hello
+            <p>bruh</p>
         </div>
     )
 }
